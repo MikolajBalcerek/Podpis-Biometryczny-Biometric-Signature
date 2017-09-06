1252b93331c9072069e822002213110b2a1f092a
Krokogator <michal26262@gmail.com>
2017-09-06 18:37:59 +0800

    Zbieranie współrzędnych i siły nacisku rysika

--- Program/PodpisBio/MainPage.xaml.cs
+++ Program/PodpisBio/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
 
 //Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
 
@@ -25,6 +28,48 @@ namespace PodpisBio
         public MainPage()
         {
             this.InitializeComponent();
+
+            //inkCanvas1.PointerMoved += canvasUpdate;
+
+            CoreInkIndependentInputSource core = CoreInkIndependentInputSource.Create(inkCanvas1.InkPresenter);
+            core.PointerPressing += Core_PointerPressing;
+            core.PointerReleasing += Core_PointerReleasing;
+            core.PointerMoving += Core_PointerMoving;
         }
+
+        private void Core_PointerMoving(CoreInkIndependentInputSource sender, PointerEventArgs args)
+        {
+            if(args.CurrentPoint.Properties.Pressure > 0.9)
+            {
+                updateInfoAsync("Adam rysuje X: " + args.CurrentPoint.Position.X + ", Y: " + args.CurrentPoint.Position.Y + ", z mocą: IT'S OVER 9000");
+            }
+            else
+            {
+                updateInfoAsync("Adam rysuje X: " + args.CurrentPoint.Position.X + ", Y: " + args.CurrentPoint.Position.Y + ", z mocą: " + args.CurrentPoint.Properties.Pressure +", " + args.CurrentPoint.Properties.Twist);
+            }
+
+            
+        }
+
+        private void Core_PointerReleasing(CoreInkIndependentInputSource sender, PointerEventArgs args)
+        {
+            Debug.WriteLine("Adam puścił");
+        }
+
+        private void Core_PointerPressing(CoreInkIndependentInputSource sender, PointerEventArgs args)
+        {
+            Debug.WriteLine("Adam wcisnął");
+        }
+
+        private async System.Threading.Tasks.Task updateInfoAsync(String value)
+        {
+            //Updates informations asynchronously
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                label1.Text = value;
+            });
+        }
+
+
     }
 }
