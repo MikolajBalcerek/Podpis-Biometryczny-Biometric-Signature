8271b75f547ea516ffac71d0d02623ec5f123043
Krokogator <michal26262@gmail.com>
2017-09-07 13:48:03 +0200

    Adam naprawia merdżowanie

--- Program/PodpisBio/MainPage.xaml.cs
+++ Program/PodpisBio/MainPage.xaml.cs
@@ -154,10 +154,30 @@ namespace PodpisBio
 
             consoleStrokeInfo(strokes);
             addSignature(strokes);
+            createCSV(strokes);
 
             inkCanvas1.InkPresenter.StrokeContainer.Clear();
 
 
+        }
+
+        private void createCSV(IReadOnlyList<InkStroke> strokes)
+        {
+            var csv = new StringBuilder();
+            var rawCsv = "";
+            foreach (var stroke in strokes)
+            {
+                foreach (var point in stroke.GetInkPoints())
+                {
+                    var newLine = string.Format(point.Position.X.ToString() + ";" + point.Position.Y.ToString() + ";" + point.Pressure.ToString() + ";" + point.Timestamp.ToString());
+
+                    rawCsv = rawCsv + newLine + Environment.NewLine;
+
+                }
+
+                rawCsv = rawCsv + Environment.NewLine;
+            }
+            writeToFileAsync(rawCsv);
         }
 
         //Write Stroke info to debug window
@@ -190,6 +210,44 @@ namespace PodpisBio
                 signature.addStroke(stroke);
             }
             signatureController.addSignature(signature);
+        }
+
+        private async Task writeToFileAsync(String rawCsv)
+        {
+            var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+            savePicker.SuggestedStartLocation =
+                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+            // Dropdown of file types the user can save the file as
+            savePicker.FileTypeChoices.Add("Plain Text", new List<string>() { ".csv" });
+            // Default file name if the user does not type one in or select a file to replace
+            savePicker.SuggestedFileName = "New Document";
+
+            Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                // Prevent updates to the remote version of the file until
+                // we finish making changes and call CompleteUpdatesAsync.
+                Windows.Storage.CachedFileManager.DeferUpdates(file);
+                // write to file
+                await Windows.Storage.FileIO.WriteTextAsync(file, rawCsv);
+                // Let Windows know that we're finished changing the file so
+                // the other app can update the remote version of the file.
+                // Completing updates may require Windows to ask for user input.
+                Windows.Storage.Provider.FileUpdateStatus status =
+                    await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                {
+                    Debug.WriteLine("File " + file.Name + " was saved.");
+                }
+                else
+                {
+                    Debug.WriteLine("File " + file.Name + " was NOT saved.");
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Cancelled");
+            }
         }
     }
 }
