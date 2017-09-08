4676eb5a37016fb5c5c36de55ca1e63ad7c4dfd9
MikolajBalcerek <mpbalcerek+github@gmail.com>
2017-09-08 11:04:46 +0200

    TimeSize class

---
+++ Program/PodpisBio/Src/Author/Signature/SignatureProbes/TimeSize Probe.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodpisBio.Src.Author.Signature.SignatureProbes
+{
+    class TimeSize_Probe
+    {
+    }
+}
