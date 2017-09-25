76a256e9e45360ca866e4175be8a0050b8edaf25
m1kra <mieczyslaw.krawiarz@gmail.com>
2017-09-25 02:50:33 +0200

    Adam oczyszcza dane

--- Program/PodpisBio/Src/FinalScore/PreciseComparison/DataPreparation.cs
+++ Program/PodpisBio/Src/FinalScore/PreciseComparison/DataPreparation.cs
@@ -14,13 +14,67 @@ namespace PodpisBio.Src.FinalScore.PreciseComparison
 
         const string NORMALISATION = "std"; // druga opcja to "minmax"
 
-        private IEnumerable<float> removeOdds(IEnumerable<float> feature)
+        private float calcStd(float average, IEnumerable<float> list)
+        {
+            var variance = (from x in list select (x - average) * (x - average)).Sum() / (list.Count() - 1);
+            return (float)Math.Sqrt(variance);
+        }
+
+        private bool outlier(float x, float avg, float std)
+        {
+            return (x - avg) > TOLLERANCE_STD * std || (x - avg) < -1 * TOLLERANCE_STD * std;
+        }
+
+        private IEnumerable<float> removeOddsFeature(IEnumerable<float> feature)
         {
             var avg = feature.Average();
-            var variance = (from x in feature select (x - avg) * (x - avg)).Sum() / (feature.Count() - 1);
-            var std = Math.Sqrt(variance);
-            return feature.Where(x => (x - avg) < TOLLERANCE_STD * std && (x - avg) > -1 * TOLLERANCE_STD * std).ToList();
+            var std = calcStd(avg, feature);
+            return feature.Where(x => !outlier(x, avg, std)).ToList();
         }
+
+        private IEnumerable<int> findOutlierIndices(IEnumerable<float> feature)
+            // zwraca indeksy odstających danych, uporządkowane malejąco
+        {
+            var avg = feature.Average();
+            var std = calcStd(avg, feature);
+            return  feature.Select((v, i) => new { v, i }).Where(x => outlier(x.v, avg, std)).Select(x => x.i)
+                .OrderByDescending(v => v);
+        }
+
+        private void removeAtIndices(ref List<Point> pts, ref List<Derivatives> ders, List<int> indices)
+        {
+            foreach(var i in indices)
+            {
+                pts.RemoveAt(i);
+                ders.RemoveAt(i);
+            }
+        }
+        public void removeOddsValues(ref List<Point> pts, ref List<Derivatives> ders)
+        {
+            var indices = findOutlierIndices(from x in pts select x.X).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in pts select x.Y).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in pts select x.Pressure).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in ders select x.Velocity).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in ders select x.VelocityX).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in ders select x.VelocityY).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in ders select x.Acc).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in ders select x.AccX).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            indices = findOutlierIndices(from x in ders select x.AccY).ToList();
+            removeAtIndices(ref pts, ref ders, indices);
+            //indices = findOutlierIndices(from x in ders select x.PressureChange);
+            //removeAtIndices(ref pts, ref ders, indices);
+            //ta lista nie ma sensu - 0, -1, 1
+        }
+
+
 
         private List<float> normaliseFeatureMinMax(IEnumerable<float> feature)
         {
@@ -33,17 +87,26 @@ namespace PodpisBio.Src.FinalScore.PreciseComparison
         private List<float> normaliseFeatureStd(IEnumerable<float> feature)
         {
             var avg = feature.Average();
-            var variance = (from x in feature select (x - avg) * (x - avg)).Sum() / (feature.Count() - 1);
-            var std = Math.Sqrt(variance);
+            var std = calcStd(avg, feature);
             return (from x in feature select (float)((x - avg) / std)).ToList();
         }
 
         public List<float> prepareFeature(IEnumerable<float> feature)
         {
             if (NORMALISATION == "std")
-                return normaliseFeatureStd(removeOdds(feature));
+                return normaliseFeatureStd(removeOddsFeature(feature));
             if (NORMALISATION == "minmax")
-                return normaliseFeatureMinMax(removeOdds(feature));
+                return normaliseFeatureMinMax(removeOddsFeature(feature));
+            Debug.WriteLine("Wybrano złą opcję normalizacji. Nic nie robię.");
+            return feature.ToList();
+        }
+
+        private List<float> prepareFeatureGlobal(IEnumerable<float> feature)
+        {
+            if (NORMALISATION == "std")
+                return normaliseFeatureStd(feature);
+            if (NORMALISATION == "minmax")
+                return normaliseFeatureMinMax(feature);
             Debug.WriteLine("Wybrano złą opcję normalizacji. Nic nie robię.");
             return feature.ToList();
         }
@@ -51,13 +114,13 @@ namespace PodpisBio.Src.FinalScore.PreciseComparison
         public List<Point> preparePoints(List<Point> points)
         {
             List<Point> result = new List<Point>();
-            var xs = prepareFeature(from x in points select x.X);
-            var ys = prepareFeature(from x in points select x.Y);
-            var pressures = prepareFeature(from x in points select x.Pressure);
-            var tiltXs = prepareFeature(from x in points select x.tiltX);
-            var tiltYs = prepareFeature(from x in points select x.tiltY);
+            var xs = prepareFeatureGlobal(from x in points select x.X);
+            var ys = prepareFeatureGlobal(from x in points select x.Y);
+            var pressures = prepareFeatureGlobal(from x in points select x.Pressure);
+            //var tiltXs = prepareFeature(from x in points select x.tiltX);
+            //var tiltYs = prepareFeature(from x in points select x.tiltY);
             for (int i = 0; i < points.Count(); i++)
-                result.Add(new Point(xs[i], ys[i], pressures[i], (ulong)points[i].Timestamp, tiltXs[i], tiltYs[i]));
+                result.Add(new Point(xs[i], ys[i], pressures[i], (ulong)points[i].Timestamp, 0, 0)); // tiltXs[i], tiltYs[i]));
 
             return result;
         }
@@ -65,18 +128,18 @@ namespace PodpisBio.Src.FinalScore.PreciseComparison
         public List<Derivatives> prepareDerivatives(List<Derivatives> derivatives)
         {
             List<Derivatives> result = new List<Derivatives>();
-            var v = prepareFeature(from x in derivatives select x.Velocity);
-            var vx = prepareFeature(from x in derivatives select x.VelocityX);
-            var vy = prepareFeature(from x in derivatives select x.VelocityY);
-            var a = prepareFeature(from x in derivatives select x.Acc);
-            var ax = prepareFeature(from x in derivatives select x.AccX);
-            var ay = prepareFeature(from x in derivatives select x.AccY);
-            var pc = prepareFeature(from x in derivatives select x.PressureChange);
-
+            var v = prepareFeatureGlobal(from x in derivatives select x.Velocity);
+            var vx = prepareFeatureGlobal(from x in derivatives select x.VelocityX);
+            var vy = prepareFeatureGlobal(from x in derivatives select x.VelocityY);
+            var a = prepareFeatureGlobal(from x in derivatives select x.Acc);
+            var ax = prepareFeatureGlobal(from x in derivatives select x.AccX);
+            var ay = prepareFeatureGlobal(from x in derivatives select x.AccY);
+            var pc = prepareFeatureGlobal(from x in derivatives select x.PressureChange);
+            //Debug.WriteLine("Max v to " + v.Max() + " ");
             for (int i = 0; i < derivatives.Count(); i++)
                 result.Add(new Derivatives(v[i], vx[i], vy[i], a[i], ax[i], ay[i], 0, 0, pc[i]));
             // nie uwzględniam tiltów, póki nie działają.
-
+            //Debug.WriteLine("Max result to " + result.Max(x => x.Velocity) + " ");
             return result;
         }
     }
